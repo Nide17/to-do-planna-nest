6fc97b184f4df2358c47c7a40731feafa28c860b
Nide17 <nidedrogba@gmail.com>
2023-04-08 04:53:34 +0200

    db data

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,11 @@ dotenv.config();
     // IMPORT THE SequelizeModule MODULE AND PASS IN THE CONFIGURATION
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST || 'todoplanna.cnuomylyyhwi.eu-north-1.rds.amazonaws.com',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'jesus123',
+      database: process.env.DB_DATABASE || 'todos',
       // models: [User], // THIS WILL IMPORT ONLY THE USER MODEL
       // models: [__dirname + '/**/*.model.ts'], // THIS WILL IMPORT ALL THE MODELS IN THE ENTIRE PROJECT
       autoLoadModels: true, // THIS WILL IMPORT ALL THE MODELS IN THE ENTIRE PROJECT
